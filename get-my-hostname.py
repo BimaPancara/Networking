d5d18657490bc305d8eb1a02878a93d0cd46c17c
BimaPancara <bimapancara0@gmail.com>
2023-07-11 06:12:55 +0700

    Create get-my-hostname.py
    Completed

---
+++ get-my-hostname.py
@@ -0,0 +1,9 @@
+# To get the hostname
+hostname = socket.gethostname()
+
+# Get ip addr
+ip_addr = socket.gethostbyname(hostname)
+
+# Print the hostname
+print(f"Hostname = {hostname}")
+print(f"IP Address = {ip_addr}")
